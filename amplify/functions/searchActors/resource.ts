5cd81449b34f9763363e6e9d802ca242dcd61224
Justin Joy <justin.joy@criticalmass.com>
2026-07-26 18:53:55 +0530

    directly using api key

--- amplify/functions/searchActors/resource.ts
+++ amplify/functions/searchActors/resource.ts
@@ -3,6 +3,6 @@ import { defineFunction, secret } from "@aws-amplify/backend";
 export const searchActors = defineFunction({
   name: "searchActors",
   environment: {
-    TMDB_API_KEY: secret("TMDB_API_KEY"),
+    TMDB_API_KEY: "39591816e4bf5a10da89a2a632d8e4a3",
   },
 });
